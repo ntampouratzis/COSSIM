38551a770a951a23a54f82ac7b1d27b6514f4beb
ntampouratzis <32739524+ntampouratzis@users.noreply.github.com>
2017-11-23 18:00:23 +0200

    COSSIM
    COSSIM Simulation Framework is an open-source framework that efficiently integrate a series of sub-tools that model the computing devices of the processing nodes as well as the network(s) of the parallel systems. It provides cycle accurate results by simulating the actual application and system software executed on each node, realistic communication modeling of the different networks that are employed.

---
+++ scripts/startup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# It takes 3 exactly parameters (e.g.: ./startup.sh IP Username Password) #
+
+echo "Startup HLA Server (rtig) & SynchServer in following account:"
+echo "IP : $1"
+echo "Username : $2"
+echo "Password : $3"
+
+export SSHPASS=$3
+
+sshpass -e ssh $2@$1 '(nohup rtig > cmd_rtig.out 2> cmd_rtig.out.err < /dev/null &)'
+sshpass -e ssh $2@$1 '(nohup SynchServer > cmd_synchServer.out 2> cmd_synchServer.err < /dev/null &)'
